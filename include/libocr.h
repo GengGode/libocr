8ffa2e38deae47d1fd060b9ab82d593e52ee3d8c
GengGode <GengGode@outlook.com>
2022-11-10 00:31:18 +0800

    feat:添加了直入直出的接口

--- include/libocr.h
+++ include/libocr.h
@@ -12,9 +12,12 @@ extern "C" {
 #endif
 
 // Path: inlcude\libocr.h
-LIBOCR_API int ocr_recognize_image_data(int image_width, int image_height, const char *image_data, int image_data_size, char *result, int result_size);
-LIBOCR_API int ocr_recognize_file_data(const char *image_data, int image_data_size, char *result, int result_size);
-LIBOCR_API int ocr_recognize_file(const char *image_file, char *result, int result_size);
+LIBOCR_API int ocr(const char *image_data, int image_data_size, char *result, int result_size);
+LIBOCR_API int ocr_image_data(int image_width, int image_height, const char *image_data, int image_data_size, char *result, int result_size);
+LIBOCR_API int ocr_file_data(const char *image_data, int image_data_size, char *result, int result_size);
+LIBOCR_API int ocr_file(const char *image_file, char *result, int result_size);
+// allocate memory
+LIBOCR_API const char* ocr_rec(const char *file_path, int *file_size);
 // free memory
 LIBOCR_API void free_char(char *p);
 
--- src/libocr.cpp
+++ src/libocr.cpp
@@ -5,17 +5,28 @@
 //#include "ocr/ocr.h"
 #include "ocr_manager/ocr_manager.h"
 
-int ocr_recognize_image_data(int image_width, int image_height, const char *image_data, int image_data_size, char *result, int result_size)
+int ocr(const char *image_data, int image_data_size, char *result, int result_size)
+{
+    return ocr_file_data(image_data, image_data_size, result, result_size);
+}
+int ocr_image_data(int image_width, int image_height, const char *image_data, int image_data_size, char *result, int result_size)
 {
     return libocr::recognize_image_data(image_width, image_height, image_data, image_data_size, result, result_size);
 }
-int ocr_recognize_file_data(const char *image_data, int image_data_size, char *result, int result_size)
+int ocr_file_data(const char *image_data, int image_data_size, char *result, int result_size)
 {
     return libocr::recognize_file_data(image_data, image_data_size, result, result_size);
 }
-int ocr_recognize_file(const char *image_file, char *result, int result_size)
+int ocr_file(const char *image_file, char *result, int result_size)
 {
     return libocr::recognize_file(image_file, result, result_size);
+}
+// allocate memory
+const char* ocr_rec(const char *file_path, int *file_size)
+{
+    char *buffer = new char[1024];
+    ocr_file(file_path, buffer, 1024);
+    return buffer;
 }
 
 // free memory
