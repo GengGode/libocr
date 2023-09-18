1f3c5a409b07b84ecc1bcda76fd4a4a753806951
zyxeeker <zyxeeker@gmail.com>
2023-09-18 17:53:32 +0800

    feat: 增加RGBA数据支持

--- src/ocr_manager/ocr_manager.h
+++ src/ocr_manager/ocr_manager.h
@@ -23,6 +23,7 @@ namespace libocr
         std::string recognize(cv::Mat& image);
         
         int recognize(int image_width, int image_height, const char *image_data, int image_data_size, char *result, int result_size);
+        int recognize(int image_width, int image_height, const char *image_data, unsigned int row_pitch, char *result, int result_size);
         int recognize(const char *image_data, int image_data_size, char *result, int result_size);
         int recognize(const char *image_file, char *result, int result_size);
     };
@@ -30,6 +31,11 @@ namespace libocr
     inline int recognize_image_data(int image_width, int image_height, const char *image_data, int image_data_size, char *result,
                                     int result_size){
         return libocr::ocr_manager::get_instance().recognize(image_width, image_height, image_data, image_data_size, result, result_size);
+    };
+
+    inline int recognize_image_data(int image_width, int image_height, const char *image_data, unsigned int row_pitch,
+                                    char *result, int result_size) {
+      return libocr::ocr_manager::get_instance().recognize(image_width, image_height, image_data, row_pitch, result, result_size);
     };
     
     inline int recognize_file_data(const char *image_data, int image_data_size, char *result, int result_size) {
