788dceb024ea2f7a6490566d85c72065c9e99ebc
Yu SuiXian <47711102+GengGode@users.noreply.github.com>
2023-09-23 10:42:05 +0800

    fix:修复ocr_file接口size为指针的问题

--- include/libocr/libocr.h
+++ include/libocr/libocr.h
@@ -62,43 +62,44 @@ namespace libocr
     // /// <returns>错误码</returns>
     // LIBOCR_API error_code recognize(const char *image_data, int image_data_size, char *result_buff, int buff_size);
 
-#ifdef  __cplusplus
-    extern "C" {
+#ifdef __cplusplus
+    extern "C"
+    {
 #endif
-    
-    /// <summary>
-    /// 加载默认使用的模型
-    /// </summary>
-    /// <param name="model_path">模型路径</param>
-    /// <returns>错误码</returns>
-    // LIBOCR_API error_code load_model_c(const char *model_path);
-    // LIBOCR_API error_code load_models_c(const char *model_tag, const char *model_path);
-    // LIBOCR_API error_code unload_models_c(const char *model_tag);
-    // LIBOCR_API error_code unload_model_c();
-    // LIBOCR_API error_code
-    // recognize(const char *model_tag, int image_width, int image_height, const char *image_data, int image_data_size,
-    //           char *result, int result_size);
-    // LIBOCR_API error_code recognize_file(const char *model_tag, const char *image_file, char *result, int result_size);
-    // LIBOCR_API error_code
-    // recognize_file_data(const char *model_tag, const char *image_data, int image_data_size, char *result,
-    //int result_size ) ;
-    
-    LIBOCR_API int ocr(const char *image_data, int image_data_size, char *result, int result_size);
-    LIBOCR_API int
-    ocr_image_data(int image_width, int image_height, const char *image_data, int image_data_size, char *result,
-                   int result_size);
-    LIBOCR_API int
-    ocr_image_data_rgba(int image_width, int image_height, const char *image_data, unsigned int row_pitch,
-                        char *result, int result_size);
-    LIBOCR_API int ocr_file_data(const char *image_data, int image_data_size, char *result, int result_size);
-    LIBOCR_API int ocr_file_path(const char *image_file, char *result, int result_size);
-// allocate memory
-    LIBOCR_API const char *ocr_file(const char *file_path, int *file_size);
-    LIBOCR_API const char *ocr_image(int image_width, int image_height, const char *image_data, int image_data_size);
-// free memory
-    LIBOCR_API void free_char(char *p);
 
-#ifdef  __cplusplus
+        /// <summary>
+        /// 加载默认使用的模型
+        /// </summary>
+        /// <param name="model_path">模型路径</param>
+        /// <returns>错误码</returns>
+        // LIBOCR_API error_code load_model_c(const char *model_path);
+        // LIBOCR_API error_code load_models_c(const char *model_tag, const char *model_path);
+        // LIBOCR_API error_code unload_models_c(const char *model_tag);
+        // LIBOCR_API error_code unload_model_c();
+        // LIBOCR_API error_code
+        // recognize(const char *model_tag, int image_width, int image_height, const char *image_data, int image_data_size,
+        //           char *result, int result_size);
+        // LIBOCR_API error_code recognize_file(const char *model_tag, const char *image_file, char *result, int result_size);
+        // LIBOCR_API error_code
+        // recognize_file_data(const char *model_tag, const char *image_data, int image_data_size, char *result,
+        // int result_size ) ;
+
+        LIBOCR_API int ocr(const char *image_data, int image_data_size, char *result, int result_size);
+        LIBOCR_API int
+        ocr_image_data(int image_width, int image_height, const char *image_data, int image_data_size, char *result,
+                       int result_size);
+        LIBOCR_API int
+        ocr_image_data_rgba(int image_width, int image_height, const char *image_data, unsigned int row_pitch,
+                            char *result, int result_size);
+        LIBOCR_API int ocr_file_data(const char *image_data, int image_data_size, char *result, int result_size);
+        LIBOCR_API int ocr_file_path(const char *image_file, char *result, int result_size);
+        // allocate memory
+        LIBOCR_API const char *ocr_file(const char *file_path, int file_size);
+        LIBOCR_API const char *ocr_image(int image_width, int image_height, const char *image_data, int image_data_size);
+        // free memory
+        LIBOCR_API void free_char(char *p);
+
+#ifdef __cplusplus
     }
 #endif
 }
--- src/libocr.cpp
+++ src/libocr.cpp
@@ -2,62 +2,70 @@
 // Created by GengG on 2022/11/9.
 //
 #include "libocr/libocr.h"
-//#include "ocr/ocr.h"
+// #include "ocr/ocr.h"
 #include "ocr_manager/ocr_manager.h"
 
 int _stdcall
 
-libocr::ocr(const char *image_data, int image_data_size, char *result, int result_size) {
+    libocr::ocr(const char *image_data, int image_data_size, char *result, int result_size)
+{
     return ocr_file_data(image_data, image_data_size, result, result_size);
 }
 
 int _stdcall
 
-libocr::ocr_image_data(int image_width, int image_height, const char *image_data, int image_data_size, char *result,
-                       int result_size) {
+    libocr::ocr_image_data(int image_width, int image_height, const char *image_data, int image_data_size, char *result,
+                           int result_size)
+{
     return libocr::recognize_image_data(image_width, image_height, image_data, image_data_size, result, result_size);
 }
 
 int _stdcall
 
-libocr::ocr_image_data_rgba(int image_width, int image_height, const char *image_data, unsigned int row_pitch,
-                            char *result, int result_size) {
+    libocr::ocr_image_data_rgba(int image_width, int image_height, const char *image_data, unsigned int row_pitch,
+                                char *result, int result_size)
+{
     return libocr::recognize_image_data(image_width, image_height, image_data, row_pitch, result, result_size);
 }
 
 int _stdcall
 
-libocr::ocr_file_data(const char *image_data, int image_data_size, char *result, int result_size) {
+    libocr::ocr_file_data(const char *image_data, int image_data_size, char *result, int result_size)
+{
     return libocr::recognize_file_data(image_data, image_data_size, result, result_size);
 }
 
 int _stdcall
 
-libocr::ocr_file_path(const char *image_file, char *result, int result_size) {
+    libocr::ocr_file_path(const char *image_file, char *result, int result_size)
+{
     return libocr::recognize_file(image_file, result, result_size);
 }
 
 // allocate memory
 const char *_stdcall
 
-libocr::ocr_file(const char *file_path, int *file_size) {
-    char *buffer = new char[1024];
-    ocr_file_path(file_path, buffer, 1024);
+    libocr::ocr_file(const char *file_path, int file_size)
+{
+    char *buffer = new char[file_size];
+    ocr_file_path(file_path, buffer, file_size);
     return buffer;
 }
 
 const char *_stdcall
 
-libocr::ocr_image(int image_width, int image_height, const char *image_data, int image_data_size) {
+    libocr::ocr_image(int image_width, int image_height, const char *image_data, int image_data_size)
+{
     char *buffer = new char[1024];
     ocr_image_data(image_width, image_height, image_data, image_data_size, buffer, 1024);
     return buffer;
 }
 
 // free memory
-void       _stdcall
+void _stdcall
 
-libocr::free_char(char *p) {
-    //free(p);
+    libocr::free_char(char *p)
+{
+    // free(p);
     delete p;
 }
